b8fb377e5d1158b6bac72202bc63c6ea50e99189
demoltn <alex.zerorc@gmail.com>
2025-05-09 21:05:15 +0500

    simple cat images downloader)

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"sync"
+)
+
+const Url = "https://cataas.com/cat"
+const maxLoad = 10
+
+func getCat() []byte {
+	resp, err := http.Get(Url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return body
+}
 
 func main() {
-	fmt.Println("Hello, go!")
+	var wg sync.WaitGroup
+	for i := 0; i < maxLoad; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			file := getCat()
+			err := os.WriteFile(fmt.Sprintf("cat%d.jpg", i), file, 0666)
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+	wg.Wait()
 }
